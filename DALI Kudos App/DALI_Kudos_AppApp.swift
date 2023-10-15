09b9484affc707e218d1e0e2515d721f36cb8eb4
Jonah Bard <jonahbard@gmail.com>
2023-10-15 16:29:02 -0400

    Initial Commit

---
+++ DALI Kudos App/DALI_Kudos_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DALI_Kudos_AppApp.swift
+//  DALI Kudos App
+//
+//  Created by Jonah Bard on 10/15/23.
+//
+
+import SwiftUI
+
+@main
+struct DALI_Kudos_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
